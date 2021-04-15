6899298d9fd3af04c2a44056864755b9b48edfdf
Akbar Irwanda <45300477+Irwanda04@users.noreply.github.com>
2021-04-16 06:06:47 +0700

    Tambah keterangan
    Mau tambahin copy tapi error

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 #include <sys/stat.h>
 #include <errno.h>
+#define maxSize 100
 
 bool is_suffix(char *filename, char *suffix){
   int filename_length = strlen(filename);
@@ -21,7 +22,8 @@ bool is_suffix(char *filename, char *suffix){
   return true;
 }
 
-void createFolder(char *filename, char *current_dir){
+void createFolder(char *filename, char *newName, char *current_dir){
+  pid_t child;
   int i, status;
   bool more_than_one = false; //cek apakah ada lebih dari satu pet
   for(i=0;i<strlen(filename);i++)
@@ -30,7 +32,9 @@ void createFolder(char *filename, char *current_dir){
       break;
     }
   int j;
-  char category[100] = "/"; //mengetahui kategori untuk membuat folder
+  char category[maxSize] = "/"; //mengetahui kategori untuk membuat folder
+  char name[maxSize];
+  char umur[5];
   for(j = 0; j<strlen(filename); j++){
     category[j+1] = filename[j];
     if(filename[j] == ';'){
@@ -38,30 +42,70 @@ void createFolder(char *filename, char *current_dir){
        break;
      }
   }
+  int counter = 0;j++;
+  for(;j<strlen(filename)-4; j++){
+    if(filename[j] == ';'){
+      name[counter] = '\0';
+      break;
+    }
+    name[counter++] = filename[j];
+  }
   
-  char newFolder[100];
+  counter = 0; j++;
+  for(;j<strlen(filename)-4;j++){
+    if(filename[j] == '_'){
+      break;
+    }
+    umur[counter++] = filename[j];
+  }
+  
+  umur[counter] = '\0';
+  char newFolder[maxSize];
   strcpy(newFolder, current_dir);
   strcat(newFolder, category);  //folder baru
-
+  
+  char keterangan[maxSize];
+  strcpy(keterangan, newFolder);
+  strcat(keterangan, "/keterangan.txt");
   // printf("%d\n", access(newFolder, F_OK));
+
+  char copy_file[maxSize];
+  strcpy(copy_file, current_dir);
+  // strcat(copy_file, "/\"");
+  strcat(copy_file, filename);
+  // strcat(copy_file, "\"");
+  printf("%s %s\n", copy_file, newFolder);
+
+
   if(access(newFolder, F_OK) == -1){  //folder belum ada
-    pid_t child;
     child = fork();
     if(child==0)
     {
       char *arg[] = {"mkdir", newFolder, NULL};  //command buat directory
       execv("/usr/bin/mkdir", arg);  //menjalankan mkdir dari file c
+
     }
     while ((wait(&status)) > 0);
   }
 
+  // child = fork();
+  // if(child == 0){
+  //   char *argv[] = {"cp", copy_file, newFolder, NULL};
+  //   execv("usr/bin/cp", argv);
+  // }
+  // while ((wait(&status)) > 0);
+
+  FILE *fptr = fopen(keterangan, "a");
+  fprintf(fptr, "nama : %s\numur : %s tahun\n\n", name, umur);
+  fclose(fptr);
+
   if(more_than_one){
-    char newName[100];
+    char newName[maxSize];
     i++;j=0;
     for(; i<strlen(filename); i++)
       newName[j++] = filename[i];
     newName[j] = '\0';
-    createFolder(newName, current_dir);   //panggil kembali fungsi ini dengan menghilangkan informasi pet depan
+    createFolder(newName, newName, current_dir);   //panggil kembali fungsi ini dengan menghilangkan informasi pet depan
     //printf("%s\n", newName);
   }
 }
@@ -71,7 +115,7 @@ int main(int argc, char *argv[])
   pid_t child1, child;
   int status;
   char *username = getenv("USER");  //mendapatkan username dalam format string
-  char destination[100] = "/home/", sourceZip[100];
+  char destination[maxSize] = "/home/", sourceZip[maxSize];
 
   strcat(destination,username);
   strcpy(sourceZip,destination);
@@ -105,11 +149,11 @@ int main(int argc, char *argv[])
   {
     while ((ep = readdir (dp))) {
       if(is_suffix(ep->d_name,".jpg")){
-        createFolder(ep->d_name, destination);
+        createFolder(ep->d_name, ep->d_name, destination);
       }
       else if ( !strcmp(ep->d_name, ".") || !strcmp(ep->d_name, "..") );
       else{
-        char to_remove[100];
+        char to_remove[maxSize];
         strcpy(to_remove, destination);
         strcat(to_remove,"/");
         strcat(to_remove, ep->d_name);
