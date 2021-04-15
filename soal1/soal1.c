f1f1ed4466264a2403e00f7ca260c5d37ba32803
FikaNA <67305615+FikaNA@users.noreply.github.com>
2021-04-15 22:41:21 +0700

    Update soal1.c

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -1,1 +1,28 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
+#include <sys/types.h>
 
+int main() {
+ 
+  pid_t child_id;
+  int status;
+
+  child_id = fork();
+  
+  if (child_id < 0) {
+    exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+  }
+
+  if (child_id == 0) {
+    // this is child
+    
+    char *argv[] = {"mkdir", "-p", "Fylm", "Musyik", "Pyoto", NULL};
+    execv("/bin/mkdir", argv);
+  } else {
+    // this is parent
+    while ((wait(&status)) > 0);
+    char *argv[] = {"touch", "folderku/fileku.txt", NULL};
+    execv("/usr/bin/touch", argv);
+  }
+}
