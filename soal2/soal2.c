30089a118bd7fec9b133982f6d5cec7543869b97
Akbar Irwanda <45300477+Irwanda04@users.noreply.github.com>
2021-04-16 13:56:01 +0700

    complete 2

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -10,6 +10,9 @@
 #include <errno.h>
 #define maxSize 100
 
+/*
+  fungsi untuk cek apakah punya suffix yang sama
+*/
 bool is_suffix(char *filename, char *suffix){
   int filename_length = strlen(filename);
   int suffix_length = strlen(suffix);
@@ -22,91 +25,157 @@ bool is_suffix(char *filename, char *suffix){
   return true;
 }
 
+/*
+  Fungsi untuk membuat folder baru dan memindahkan sesuai dengan soal.
+  Dengan 3 parameter yaitu nama file asli, nama file jika lebih dari
+  satu foto hewan dengan menghilangkan spesifikasi hewan yang sudah 
+  dikelompokkan dan direktori tujuan semua file.
+*/
 void createFolder(char *filename, char *newName, char *current_dir){
   pid_t child;
   int i, status;
   bool more_than_one = false; //cek apakah ada lebih dari satu pet
-  for(i=0;i<strlen(filename);i++)
-    if(filename[i] == '_'){
+  for(i=0;i<strlen(newName);i++)
+    if(newName[i] == '_'){
       more_than_one = true;
       break;
     }
   int j;
-  char category[maxSize] = "/"; //mengetahui kategori untuk membuat folder
+  
+  /*
+    Spesifikasi dari foto yang ada (kategori, nama dan umur)
+    Kategori dimulai dari "/" agar langsung digabung dengan direktori
+  */
+  char category[maxSize] = "/";
   char name[maxSize];
   char umur[5];
-  for(j = 0; j<strlen(filename); j++){
-    category[j+1] = filename[j];
-    if(filename[j] == ';'){
+  
+  /*
+  Mengetahui kategori hewan dari nama
+  */
+  for(j = 0; j<strlen(newName); j++){
+    category[j+1] = newName[j];
+    if(newName[j] == ';'){
       category[j+1] = '\0';
        break;
      }
   }
+  
+  /*
+    Mendapatkan nama dari hewan yang dimaksud
+  */
   int counter = 0;j++;
-  for(;j<strlen(filename)-4; j++){
-    if(filename[j] == ';'){
+  for(;j<strlen(newName)-4; j++){
+    if(newName[j] == ';'){
       name[counter] = '\0';
       break;
     }
-    name[counter++] = filename[j];
+    name[counter++] = newName[j];
   }
   
+  /*
+    Mendapatkan umur dari binatang peliharaan.
+  */
   counter = 0; j++;
-  for(;j<strlen(filename)-4;j++){
-    if(filename[j] == '_'){
+  for(;j<strlen(newName)-4;j++){
+    if(newName[j] == '_'){
       break;
     }
-    umur[counter++] = filename[j];
+    umur[counter++] = newName[j];
   }
-  
   umur[counter] = '\0';
+  
+  /*
+    Folder baru untuk karegori tiap binatang dengan menggabungkan 
+    direktori saat ini dengan nama kategori
+  */
   char newFolder[maxSize];
   strcpy(newFolder, current_dir);
   strcat(newFolder, category);  //folder baru
-  
+
+  /*
+    Membuat file txt berisi keterangan yang dimasukkan pada folder tiap kategori,
+    yaitu newFolder.
+  */
   char keterangan[maxSize];
   strcpy(keterangan, newFolder);
   strcat(keterangan, "/keterangan.txt");
   // printf("%d\n", access(newFolder, F_OK));
 
+  /*
+    Path dari nama file untuk melakukan copy
+  */
   char copy_file[maxSize];
   strcpy(copy_file, current_dir);
-  // strcat(copy_file, "/\"");
+  strcat(copy_file, "/");
   strcat(copy_file, filename);
-  // strcat(copy_file, "\"");
-  printf("%s %s\n", copy_file, newFolder);
 
+  /*
+    Path tujuan dari copy dengan langsung mengubah namanya menjadi
+    [nama].jpg
+  */
+  char newPath[maxSize];
+  strcpy(newPath, newFolder);
+  strcat(newPath, "/");
+  strcat(newPath, name);
+  strcat(newPath, ".jpg");
 
+  /*
+    Membuat folder baru untuk kategori dengan memeriksa apakah folder
+    sudah ada atau belum
+  */
   if(access(newFolder, F_OK) == -1){  //folder belum ada
     child = fork();
     if(child==0)
     {
       char *arg[] = {"mkdir", newFolder, NULL};  //command buat directory
       execv("/usr/bin/mkdir", arg);  //menjalankan mkdir dari file c
-
     }
     while ((wait(&status)) > 0);
   }
 
-  // child = fork();
-  // if(child == 0){
-  //   char *argv[] = {"cp", copy_file, newFolder, NULL};
-  //   execv("usr/bin/cp", argv);
-  // }
-  // while ((wait(&status)) > 0);
+  /*
+    Copy file dari path file sumber ke path rujuan
+  */
+  child = fork();
+  if(child == 0){
+    char *argv[] = {"cp", copy_file, newPath};
+    execlp("cp", argv[0], argv[1], argv[2], NULL);
+  }
+  while ((wait(&status)) > 0);
 
+  /*
+    Mengisi nama dan umur di keterangan.txt tiap folder
+  */
   FILE *fptr = fopen(keterangan, "a");
   fprintf(fptr, "nama : %s\numur : %s tahun\n\n", name, umur);
   fclose(fptr);
 
+  /*
+    Jika lebih dari satu pet, lakukan rekursi karena tidak pasti
+    berapa jumlah pet dalam satu foto.
+  */
   if(more_than_one){
     char newName[maxSize];
     i++;j=0;
-    for(; i<strlen(filename); i++)
-      newName[j++] = filename[i];
+    for(; i<strlen(filename); i++, j++)
+      newName[j] = filename[i];
     newName[j] = '\0';
-    createFolder(newName, newName, current_dir);   //panggil kembali fungsi ini dengan menghilangkan informasi pet depan
+    createFolder(filename, newName, current_dir);   //panggil kembali fungsi ini dengan menghilangkan informasi pet depan
     //printf("%s\n", newName);
+  }
+  
+  /*
+    Jika sudah copy semua file, hapus.
+    Dari awal tidak menggunakan move karena ada yang lebih dari satu pet
+  */
+  else{
+    child = fork();
+    if(child == 0){
+      char *remove[] = {"rm", copy_file, NULL};
+      execv("/usr/bin/rm", remove);
+    }
+    while ((wait(&status)) > 0);
   }
 }
 
@@ -119,7 +188,6 @@ int main(int argc, char *argv[])
 
   strcat(destination,username);
   strcpy(sourceZip,destination);
-
   strcat(sourceZip,"/Downloads/pets.zip"); //directory pets.zip
   strcat(destination,"/modul2");
 
@@ -148,11 +216,20 @@ int main(int argc, char *argv[])
   if (dp != NULL)
   {
     while ((ep = readdir (dp))) {
+
+      /*
+        Jika berakhiran jpg, artinya file yang ditunjuk merupakan file gambar/foto
+      */
       if(is_suffix(ep->d_name,".jpg")){
         createFolder(ep->d_name, ep->d_name, destination);
       }
-      else if ( !strcmp(ep->d_name, ".") || !strcmp(ep->d_name, "..") );
+      
+      else if ( !strcmp(ep->d_name, ".") || !strcmp(ep->d_name, "..") );  //Folder sekarang atau folder parent, jadi biarkan saja
+      
       else{
+        /*
+          hapus folder yang tidak digunakan
+        */
         char to_remove[maxSize];
         strcpy(to_remove, destination);
         strcat(to_remove,"/");
